f073bf18babd73ab5cd7017300b899caf0721259
most5554 <mostafamohamed@aiesec.net>
2024-07-07 00:13:06 +0300

    Add files via upload

---
+++ Photos.js
@@ -0,0 +1,16 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[slideIndex - 1].style.display = 'block';
+}
